8f921ae08d41646fa79f24443cc83080b2aa50a1
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-02-21 19:55:35 -0300

    productDetail defined

--- detailProduct.php
+++ detailProduct.php
@@ -1,101 +1,68 @@
+<?php
+$id = mysqli_real_escape_string($con, $_REQUEST['id'] ?? '');
+$queryProduct = "SELECT id,name,price,quantity FROM products where id='$id';  ";
+$resProduct = mysqli_query($con, $queryProduct);
+$rowProduct = mysqli_fetch_assoc($resProduct);
+
+$fmt = new NumberFormatter('es_ES', NumberFormatter::CURRENCY);
+?>
+
 <!-- Default box -->
 <div class="card card-solid">
     <div class="card-body">
         <div class="row">
             <div class="col-12 col-sm-6">
-                <h3 class="d-inline-block d-sm-none">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
+                <h3 class="d-inline-block d-sm-none"><?php echo $rowProducto['name'] ?></h3>
+                <?php
+                $queryImages = "SELECT 
+                f.web_path
+                FROM
+                products AS p
+                INNER JOIN products_files AS pf ON pf.product_id=p.id
+                INNER JOIN files AS f ON f.id=pf.file_id
+                WHERE p.id='$id';
+                ";
+                $resFirstImg = mysqli_query($con, $queryImages);
+                $rowFirstImg = mysqli_fetch_assoc($resFirstImg);
+                ?>
                 <div class="col-12">
-                    <img src="admin/dist/img/prod-1.jpg" class="product-image" alt="Product Image">
+                    <img src="<?php echo $rowFirstImg['web_path'] ?>" class="product-image">
                 </div>
                 <div class="col-12 product-image-thumbs">
-                    <div class="product-image-thumb active"><img src="admin/dist/img/prod-1.jpg" alt="Product Image"></div>
-                    <div class="product-image-thumb"><img src="admin/dist/img/prod-2.jpg" alt="Product Image"></div>
-                    <div class="product-image-thumb"><img src="admin/dist/img/prod-3.jpg" alt="Product Image"></div>
-                    <div class="product-image-thumb"><img src="admin/dist/img/prod-4.jpg" alt="Product Image"></div>
-                    <div class="product-image-thumb"><img src="admin/dist/img/prod-5.jpg" alt="Product Image"></div>
+                    <?php
+                    $resImages = mysqli_query($con, $queryImages);
+                    while ($rowImages = mysqli_fetch_assoc($resImages)) {
+                    ?>
+
+                        <div class="product-image-thumb"><img src="<?php echo $rowImagenes['web_path'] ?>"></div>
+                    <?php
+                    }
+                    ?>
                 </div>
             </div>
             <div class="col-12 col-sm-6">
-                <h3 class="my-3">LOWA Men’s Renegade GTX Mid Hiking Boots Review</h3>
-                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terr.</p>
+                <h3 class="my-3"><?php echo $rowProduct['name'] ?></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi soluta in laborum! Fugit vero placeat quia sapiente iure cum, mollitia eligendi qui ipsam, quasi deleniti odit aut reprehenderit laudantium?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi soluta in laborum! Fugit vero placeat quia sapiente iure cum, mollitia eligendi qui ipsam, quasi deleniti odit aut reprehenderit laudantium?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi soluta in laborum! Fugit vero placeat quia sapiente iure cum, mollitia eligendi qui ipsam, quasi deleniti odit aut reprehenderit laudantium?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sequi soluta in laborum! Fugit vero placeat quia sapiente iure cum, mollitia eligendi qui ipsam, quasi deleniti odit aut reprehenderit laudantium?</p>
                 <hr>
-                <h4>Available Colors</h4>
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label class="btn btn-default text-center active">
-                        <input type="radio" name="color_option" id="color_option1" autocomplete="off" checked="">
-                        Green
-                        <br>
-                        <i class="fas fa-circle fa-2x text-green"></i>
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option2" autocomplete="off">
-                        Blue
-                        <br>
-                        <i class="fas fa-circle fa-2x text-blue"></i>
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option3" autocomplete="off">
-                        Purple
-                        <br>
-                        <i class="fas fa-circle fa-2x text-purple"></i>
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option4" autocomplete="off">
-                        Red
-                        <br>
-                        <i class="fas fa-circle fa-2x text-red"></i>
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option5" autocomplete="off">
-                        Orange
-                        <br>
-                        <i class="fas fa-circle fa-2x text-orange"></i>
-                    </label>
-                </div>
-                <h4 class="mt-3">Size <small>Please select one</small></h4>
-                <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option1" autocomplete="off">
-                        <span class="text-xl">S</span>
-                        <br>
-                        Small
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option1" autocomplete="off">
-                        <span class="text-xl">M</span>
-                        <br>
-                        Medium
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option1" autocomplete="off">
-                        <span class="text-xl">L</span>
-                        <br>
-                        Large
-                    </label>
-                    <label class="btn btn-default text-center">
-                        <input type="radio" name="color_option" id="color_option1" autocomplete="off">
-                        <span class="text-xl">XL</span>
-                        <br>
-                        Xtra-Large
-                    </label>
-                </div>
+                <h4>Quantity: <?php echo $rowProduct['quantity'] ?></h4>
+
+
                 <div class="bg-gray py-2 px-3 mt-4">
                     <h2 class="mb-0">
-                        $80.00
+                        <?php echo $fmt->formatCurrency($rowProduct['price'], 'EUR'); 
+                        ?>
                     </h2>
-                    <h4 class="mt-0">
-                        <small>Ex Tax: $80.00 </small>
-                    </h4>
+
                 </div>
                 <div class="mt-4">
                     <div class="btn btn-primary btn-lg btn-flat">
                         <i class="fas fa-cart-plus fa-lg mr-2"></i>
                         Add to Cart
                     </div>
-                    <div class="btn btn-default btn-lg btn-flat">
-                        <i class="fas fa-heart fa-lg mr-2"></i>
-                        Add to Wishlist
-                    </div>
+
                 </div>
                 <div class="mt-4 product-share">
                     <a href="#" class="text-gray">
